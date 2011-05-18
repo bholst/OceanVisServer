f26d6d5687fe1bb84e20aab175e360a8ff482de0
Bastian Holst <bastianholst@gmx.de>
2011-05-18 13:58:21 +0200

    Removing unnecessary whitespaces.

--- src/lib/DimensionSubset.h
+++ src/lib/DimensionSubset.h
@@ -17,21 +17,21 @@ public:
     DimensionSubset(Dimension dimension);
     DimensionSubset(const DimensionSubset &other);
     DimensionSubset(DimensionSubsetPrivate *d);
-    
+
     virtual ~DimensionSubset();
-    
+
     Dimension dimension() const;
-    
+
     DimensionSubset& operator=(const DimensionSubset &other);
     bool operator==(const DimensionSubset &other) const;
-    
+
     void detach();
-    
+
 protected:
     void ensureDimensionType(const QVariant& value) const throw (BadDimensionTypeException);
 
     DimensionSubsetPrivate *d;
-    
+
 private:
     DimensionSubsetPrivate *p() const;
 };
