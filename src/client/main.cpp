57c120d780f72a753aba0479d6cc99762913ea4f
Bastian Holst <bastianholst@gmx.de>
2011-07-31 13:10:05 +0200

    Started on the NEW client.

---
+++ src/client/main.cpp
@@ -0,0 +1,17 @@
+//
+// Copyright 2011      Bastian Holst <bastianholst@gmx.de>
+//
+
+// STD
+#include <iostream>
+
+// Qt
+#include <QtCore/QDebug>
+#include <QApplication>
+
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    return app.exec();
+}
